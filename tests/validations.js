581ed84612ca4a9f188f7368d73d730175fb19be
Mike Lyons <mdl0394@gmail.com>
2014-12-21 06:26:40 -0500

    Added Validations for path

--- tests/validations.js
+++ tests/validations.js
@@ -15,7 +15,8 @@
         };
 
         var valid_in = {
-            method: 'get'
+            method: 'get',
+            path: '/test'
         };
 
         var valid_out = {
@@ -28,7 +29,7 @@
 
             assert.throws( function() {
                 trust( valid_done, invalid_in, valid_out );
-            }, 'in_options: method not specified: expected {} to have a property \'method\'' );
+            }, /in_options: method not specified/ );
         } );
 
         it( 'Should not be valid when no method is not a string', function() {
@@ -37,16 +38,56 @@
 
             assert.throws( function() {
                 trust( valid_done, invalid_in, valid_out );
-            }, 'in_options: method not a string: expected 1 to be a string' );
+            }, /in_options: method not a string/ );
         } );
 
-        it( 'Should not be valid when no method is not a string', function() {
+        it( 'Should not be valid when method is not in valid methods list', function() {
             var invalid_in = _.clone( valid_in );
             invalid_in.method = 'NOT CORRECT';
 
             assert.throws( function() {
                 trust( valid_done, invalid_in, valid_out );
-            }, 'in_options: method not valid method: expected [ \'get\', \'put\', \'post\', \'delete\' ] to include \'NOT CORRECT\'' );
+            }, /in_options: method not valid method/ );
+        } );
+
+        it( 'Should work with all valid methods', function() {
+            var other_methods = [ 'get', 'put', 'post', 'delete' ];
+
+            _.each( other_methods, function( other_method ) {
+                var other_method_in = _.clone( valid_in );
+                other_method_in.method = other_method;
+
+                assert.doesNotThrow( function() {
+                    trust( valid_done, other_method_in, valid_out );
+                } );
+            } );
+        } );
+
+        it( 'Should not be valid when no path specified', function() {
+            var invalid_in = _.clone( valid_in );
+            delete invalid_in.path;
+
+            assert.throws( function() {
+                trust( valid_done, invalid_in, valid_out );
+            }, /in_options: path not specified/ );
+        } );
+
+        it( 'Should not be valid when no path is not a string', function() {
+            var invalid_in = _.clone( valid_in );
+            invalid_in.path = 1;
+
+            assert.throws( function() {
+                trust( valid_done, invalid_in, valid_out );
+            }, /in_options: path not a string/ );
+        } );
+
+        it( 'Should not be valid when path does not start with /', function() {
+            var invalid_in = _.clone( valid_in );
+            invalid_in.path = 'NOT CORRECT';
+
+            assert.throws( function() {
+                trust( valid_done, invalid_in, valid_out );
+            }, /in_options: path doesn't start with \// );
         } );
     } );
 }) ();
