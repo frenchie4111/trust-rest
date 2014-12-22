5155a87eb4d82a119fd0aa903bdee088e7a09872
Mike Lyons <mdl0394@gmail.com>
2014-12-21 19:59:52 -0500

    Added _prepareRequest and _addValidations

--- tests/test.js
+++ tests/test.js
@@ -7,13 +7,36 @@
         assert = chai.assert,
         helper = require( './lib/helper' );
 
+    var trust_lib = require( '../' );
+
     describe( 'Smoke test', function() {
         before( function( done ) {
             helper.before().then( done );
         } );
 
-        it( 'Should true', function() {
-            assert( true );
+        it( 'Should work when valid', function( done ) {
+            var trust = trust_lib( 'http://127.0.0.1:' + helper.port );
+
+            var completion_handler = function( err ) {
+                assert.isUndefined( err );
+                done();
+            };
+
+            assert.doesNotThrow( function() {
+                trust( completion_handler, {
+                    path: '/test',
+                    method: 'get'
+                }, {
+                    code: 200,
+                    content_type: /json/,
+                    body: {
+                        test: {
+                            required: true,
+                            value: 'test'
+                        }
+                    }
+                } );
+            } );
         } );
 
         after( function( done ) {
